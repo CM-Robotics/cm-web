2ac9929f832c1c6aca6df0a3460254197886ae7a
Billy <Billy@XAVIER>
2017-09-14 21:09:56 -0500

    Add project files.

---
+++ cm web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cm_web.MvcApplication" Language="C#" %>
---
+++ cm web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cm_web.Startup))]
+namespace cm_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
